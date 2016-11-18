faa55fe4adbbdcc702b3a696bbc08e567b726870
Mattia46 <mattia.assogna@gmail.com>
2016-11-18 17:26:39 +0000

    @structure: added the import component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 //import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
-class App extends React.Component {
+export class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
       </div>
     );
   }
 }
 
-export default App;
+export class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
 
+  handleLoginClick() {
+    this.setState({isLoggedIn: true})
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false})
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button = null;
+    if(isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+      <Greeting isLoggedIn={isLoggedIn} />
+      {button}
+      </div>
+    );
+  }
+}
 
--- src/index.js
+++ src/index.js
@@ -1,36 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { App, LoginControl } from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
 
-function Tick(){
-	ReactDOM.render (
-			<Clock />,
-			document.getElementById('helloformat')
-			)
-}
-
-setInterval(Tick, 1000);
+//setInterval(Tick, 1000);
 
 //CLOCK IN ES6
-class Clock extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {date: new Date()}
-	}
-	render(){
-		return (
-			<div>
-			<h3> New Time: { this.state.date.toLocaleTimeString() } </h3>
-			</div>
-	       );
-	}
+class Toggle extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {isToggleOn: true};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render(){
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON':'OFF'}
+      </button>
+    );
+  }
 }
+
+  function UserGreeting(props) {
+    return <h1> Welcome </h1>;
+  }
+
+  function GuestGreeting(props) {
+    return <h1> Not Welcome </h1>;
+  }
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if(isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+    this.test();
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render(){
+    return (
+      <div>
+      <h3> New Time: { this.state.date.toLocaleTimeString() } </h3>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  //<Greeting isLoggedIn={true} />,
+  document.getElementById('root')
+);
+
+ReactDOM.render (
+  <Clock />,
+  document.getElementById('helloformat')
+);
+
+ReactDOM.render (
+  <Toggle />,
+  document.getElementById('test')
+)
 
